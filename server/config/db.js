e38571cbafbe6e67cd618539ef31017be7cc65c5
afbryde <sheila.535220028@stu.untar.ac.id>
2024-05-02 21:52:44 +0700

    perbaikkan
    ngubah mongodb offline jadi online

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,20 @@
 const mongoose = require("mongoose");
+
+const UserModel = require("../models/UserModel.js");
+const { sample_users } = require("../sample_data/data.js");
+
 mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`Database Connected: ${conn.connection.host}`);
+    
+    const userCount = await UserModel.countDocuments();
+
+    if (userCount == 0) {
+      await UserModel.insertMany(sample_users);
+    }
   } catch (error) {
     console.log(error);
   }
